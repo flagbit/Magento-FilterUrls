697d70bbbe82a054d9bddaf1a52c20b06d96a439
Damian Luszczymak <info@icyapp.de>
2012-08-14 10:59:29 +0200

    Kommentar wegen Veröffentlichung entfernt

--- src/app/code/community/Flagbit/FilterUrls/Model/Catalog/Layer.php
+++ src/app/code/community/Flagbit/FilterUrls/Model/Catalog/Layer.php
@@ -2,10 +2,6 @@
 
 class Flagbit_FilterUrls_Model_Catalog_Layer extends Mage_Catalog_Model_Layer
 {
-//     SELECT DISTINCT `e`.`attribute_set_id` FROM `catalog_product_entity` AS `e`
-//     INNER JOIN `catalog_category_product_index` AS `cat_index` ON cat_index.product_id=e.entity_id AND cat_index.store_id='1' AND cat_index.visibility IN(2, 4) AND cat_index.category_id='10'
-//     INNER JOIN `catalog_product_index_price` AS `price_index` ON price_index.entity_id = e.entity_id AND price_index.website_id = '1' AND price_index.customer_group_id = 0
-
     /**
     * Get attribute sets identifiers of current product set
     *
