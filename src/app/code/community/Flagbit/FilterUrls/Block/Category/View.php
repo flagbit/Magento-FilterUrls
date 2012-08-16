39f4ca100780ac657ebe9ca00903bce621989053
unknown <damian.luszczymak@LAFLA1001.flagbit.local>
2012-08-16 12:11:40 +0200

    [+FEATURE] Canonical URL in category tree

---
+++ src/app/code/community/Flagbit/FilterUrls/Block/Category/View.php
@@ -0,0 +1,43 @@
+<?php
+
+class Flagbit_FilterUrls_Block_Category_View extends Mage_Catalog_Block_Category_View
+{
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+
+        $this->getLayout()->createBlock('catalog/breadcrumbs');
+
+        if ($headBlock = $this->getLayout()->getBlock('head')) {
+            $category = $this->getCurrentCategory();
+            if ($title = $category->getMetaTitle()) {
+                $headBlock->setTitle($title);
+            }
+            if ($description = $category->getMetaDescription()) {
+                $headBlock->setDescription($description);
+            }
+            if ($keywords = $category->getMetaKeywords()) {
+                $headBlock->setKeywords($keywords);
+            }
+            if ($this->helper('catalog/category')->canUseCanonicalTag()) {
+                $rewrite = Mage::getStoreConfig('web/seo/use_rewrites',Mage::app()->getStore()->getId());
+                // Rewrite
+                if($rewrite == 1) {
+                    $headBlock->addLinkRel('canonical', Mage::getModel('filterurls/catalog_layer_filter_item')->getSpeakingFilterUrl(FALSE, TRUE));
+                }
+                else{
+                    $headBlock->addLinkRel('canonical', $category->getUrl());
+                }
+            }
+            /*
+            want to show rss feed in the url
+            */
+            if ($this->IsRssCatalogEnable() && $this->IsTopCategory()) {
+                $title = $this->helper('rss')->__('%s RSS Feed',$this->getCurrentCategory()->getName());
+                $headBlock->addItem('rss', $this->getRssLink(), 'title="'.$title.'"');
+            }
+        }
+        return $this;
+    }
+
+}
