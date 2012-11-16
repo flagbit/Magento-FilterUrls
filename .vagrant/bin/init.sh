ff94c83efe8548e2f6472467a1c5fe1c8cde32e5
Karl Spies <Karl.Spies@flagbit.de>
2012-11-16 16:36:56 +0100

    [~TASK] changed vagrant files to fit into ubuntu enviroment and to get phpmyadmin access.

--- .vagrant/bin/init.sh
+++ .vagrant/bin/init.sh
@@ -31,8 +31,8 @@ mkdir -p ${checkFile}
 #########Install Extensions ####################################################
 
 for i in /usr/local/src/vagrant/extensions/*.tgz; do tar xfz $i -C /usr/local/src/magento/ext/${magentoVersion}; done
-sudo chmod -R 777 /usr/local/src/magento/ext/${magentoVersion}    
-sudo chown vagrant:www-data -R /usr/local/src/magento/ext/${magentoVersion}  
+sudo chmod -R 777 /usr/local/src/magento/ext/${magentoVersion}
+sudo chown vagrant:www-data -R /usr/local/src/magento/ext/${magentoVersion}
 
 checkFile="/usr/local/src/vagrant/dump/dump.tgz"
 checkDir="/usr/local/src/magento/live"
@@ -44,32 +44,32 @@ if ( [ -f ${checkFile} ] && [ ! -d ${checkDir} ] ); then
 	echo [+] preparing Userights
 	livePath="/usr/local/src/magento/live"
 	sudo chmod o+w ${livePath}/var ${livePath}/var/.htaccess ${livePath}/app/etc
-	sudo chmod -R 775 ${livePath}/media ${livePath}/var     
+	sudo chmod -R 775 ${livePath}/media ${livePath}/var
 	echo [+] cleaning up Magento
-	sudo chown vagrant:www-data ${livePath}/* -R   
+	sudo chown vagrant:www-data ${livePath}/* -R
 fi
 
 checkFile="/usr/local/src/vagrant/dump/dump.sql"
 
 if [ -f ${checkFile} ]; then
 	echo [-] ${checkFile} exists
-	
+
 	echo [+] import database dump? \(y\|n\)
 	read importFile
-	
+
 	if [ "$importFile" == "y" ]; then
 		echo [+] import live Database
-		
+
 		mysql=`which mysql`
-		
+
 		${mysql} -u ${magentoVersion} -pvagrant1 ${magentoVersion} < ${checkFile}
-		
+
 	    sql="UPDATE core_config_data SET value = 'http://localhost:8080/${magentoVersion}/' WHERE path LIKE 'web/%/base_url';"
 	    ${mysql} -u${magentoVersion} -pvagrant1 ${magentoVersion} -e "${sql}"
-	    
+
 	    sql="UPDATE core_config_data SET value = '.localhost' WHERE path LIKE 'web/%/cookie_domain';"
 		${mysql} -u${magentoVersion} -pvagrant1 ${magentoVersion} -e "${sql}"
-	    
+
 	    sql="UPDATE core_config_data SET value = 'http://localhost:8080/${magentoVersion}/media/' WHERE path LIKE 'web/%/base_media_url';"
 		${mysql} -u${magentoVersion} -pvagrant1 ${magentoVersion} -e "${sql}"
 	fi
@@ -89,10 +89,12 @@ echo [+] mount /usr/local/src/magento/versions/${magentoVersion}/ /var/www/${mag
 sudo mount -o remount,append:/usr/local/src/magento/ext/${magentoVersion} /var/www/${magentoVersion}/
 echo [+] mount /usr/local/src/magento/ext/${magentoVersion} /var/www/${magentoVersion}/
 
+sudo ln -s /usr/share/phpmyadmin /var/www/phpmyadmin
+echo [+] linking phpmyadmin to /var/www/phpmyadmin
+
 #sudo mount -o remount,append:/usr/local/src/magento/live/ /var/www/${magentoVersion}/
 #echo [+] mount /usr/local/src/magento/live/ /var/www/${magentoVersion}/
 
- 
 
 
-	
+
