c45b7352cd6970832a5009c6385bd49a0c92bd2f
Karl Spies <Karl.Spies@flagbit.de>
2012-11-16 15:25:55 +0100

    [+FEATURE] added support to generate sitemap for filter urls. Refs: MAGEFIL-7

---
+++ src/app/code/community/Flagbit/FilterUrls/controllers/SitemapController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of the Flagbit_FilterUrls project.
+ *
+ * Flagbit_FilterUrls is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * PHP version 5
+ *
+ * @category Flagbit_FilterUrls
+ * @package Flagbit_FilterUrls
+ * @author Karl Spies <karl.spies@flagbit.de>
+ * @copyright 2012 Flagbit GmbH & Co. KG (http://www.flagbit.de). All rights served.
+ * @license http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version 0.2.0
+ * @since 0.2.0
+ */
+/**
+ * Controller to display all generated urls.
+ *
+ * @category Flagbit_FilterUrls
+ * @package Flagbit_FilterUrls
+ * @author Karl Spies <karl.spies@flagbit.de>
+ * @copyright 2012 Flagbit GmbH & Co. KG (http://www.flagbit.de). All rights served.
+ * @license http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version 0.2.0
+ * @since 0.2.0
+ */
+class Flagbit_FilterUrls_SitemapController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
